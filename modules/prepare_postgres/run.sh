8213ae187e7449a13e527b9e84e0777a458b43e6
tyler <1851017+TylerHendrickson@users.noreply.github.com>
2024-10-22 20:18:28 +0000

    fix shell quoting

--- modules/prepare_postgres/run.sh
+++ modules/prepare_postgres/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # NOTE: If a db already exist, its "psql create database" command will fail; that's ok!
-psql -c "alter user '${PGUSER}' with password '${PGPASSWORD}'"
+psql -c "alter user \"${PGUSER}\" with password '${PGPASSWORD}'"
 
 %{ for dbname in DB_NAMES ~}
-psql -c "create database '${dbname}'"
+psql -c "create database \"${dbname}\""
 %{ endfor ~}

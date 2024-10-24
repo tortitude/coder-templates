1707f07171b2def7cfed49f3b4c4198ea7f4e1cc
tyler <1851017+TylerHendrickson@users.noreply.github.com>
2024-10-24 16:13:33 +0000

    Fix shell condition

--- modules/prepare_postgres/run.sh
+++ modules/prepare_postgres/run.sh
@@ -4,10 +4,9 @@ PGPASSWORD="${PGPASSWORD}"
 
 psql -c "alter user \"${PGUSER}\" with password '$PGPASSWORD'"
 
-# NOTE: If a db already exist, its "psql create database" command will fail; that's ok!
 %{ for DBNAME in DB_NAMES ~}
 DBNAME="${DBNAME}"
-if [ $(psql -XtAc "SELECT 1 FROM pg_database WHERE datname='$DBNAME'") != '1' ]; then
+if [ "$(psql -XtAc "SELECT 1 FROM pg_database WHERE datname='$DBNAME'")" != '1' ]; then
     echo "Creating database $DBNAME..."
     psql -c "create database \"$DBNAME\" with owner \"$PGUSER\""
 else

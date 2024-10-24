ef348e35c2e906227acd58ece2e18fad4000306b
tyler <1851017+TylerHendrickson@users.noreply.github.com>
2024-10-24 15:39:31 +0000

    Formatting

--- modules/build_dotenv/run.sh
+++ modules/build_dotenv/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-%{for waiter in WAIT_FOR ~}
+%{ for waiter in WAIT_FOR ~}
 WAIT_PATH="${waiter.path}"
 WAIT_PATH="$${WAIT_PATH/#\~/$${HOME}}"
 WAIT_INTERVAL="${waiter.interval}"
@@ -8,7 +8,7 @@ WAIT_STARTED_AT=$(date '+%s')
 
 echo "Waiting for $WAIT_PATH to exist..."
 while [ ! -e "$WAIT_PATH" ]; do
-%{if coalesce(waiter.timeout, 0) > 0 ~}
+%{ if coalesce(waiter.timeout, 0) > 0 ~}
     WAIT_TIMEOUT="${waiter.timeout}"
     WAIT_INTERVAL="${max(coalesce(try(parseint(waiter.interval), 1), 1), 1)}"
     WAIT_ELAPSED=$(($(date '+%s') - $WAIT_STARTED_AT))
@@ -16,35 +16,35 @@ while [ ! -e "$WAIT_PATH" ]; do
         echo "FAIL: Path $WAIT_PATH not found after $WAIT_TIMEOUT seconds" > /dev/stderr
         exit 1
     fi
-%{endif ~}
+%{ endif ~}
     sleep $WAIT_INTERVAL
 done
 
-%{endfor ~}
+%{ endfor ~}
 
 TARGET_DOTENV="${TARGET_DOTENV}"
 TARGET_DOTENV="$${TARGET_DOTENV/#\~/$${HOME}}"
 
-%{if ALLOW_OVERWRITE != "true" ~}
+%{ if ALLOW_OVERWRITE != "true" ~}
 # Exit early if the target file already exists
 if [ -f "$TARGET_DOTENV" ]; then
     exit 0
 fi
-%{endif ~}
+%{ endif ~}
 
-%{if SOURCE_DOTENV != "" ~}
+%{ if SOURCE_DOTENV != "" ~}
 # Copy from a source file (e.g. .env.example)
 SOURCE_DOTENV="${SOURCE_DOTENV}"
 SOURCE_DOTENV="$${SOURCE_DOTENV/#\~/$${HOME}}"
 echo "Copying $SOURCE_DOTENV to $TARGET_DOTENV..."
 cp "$SOURCE_DOTENV" "$TARGET_DOTENV"
-%{endif ~}
+%{ endif ~}
 
 # Replace env vars or append them if they are not present
 echo "Building dotenv file $TARGET_DOTENV..."
 %{ for key, value in ENV_VARS ~}
 sed -i 's|${key}=.*|${key}=${value}|' "$TARGET_DOTENV"
-grep -E '^${key}=' "$TARGET_DOTENV" || printf '%s=%s\n' '${key}' '${value}' >>"$TARGET_DOTENV"
+grep -E '^${key}=' "$TARGET_DOTENV" > /dev/null || printf '%s=%s\n' '${key}' '${value}' >> "$TARGET_DOTENV"
 %{ endfor ~}
 
 echo "Built dotenv file $TARGET_DOTENV"

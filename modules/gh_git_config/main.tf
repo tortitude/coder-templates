f97caa2b36454ab24d53856ee956ec380edf909e
TylerHendrickson <1851017+TylerHendrickson@users.noreply.github.com>
2024-10-24 23:06:16 +0000

    formatting

--- modules/gh_git_config/main.tf
+++ modules/gh_git_config/main.tf
@@ -14,15 +14,15 @@ variable "agent_id" {
   type        = string
 }
 
+variable "external_auth_id" {
+  description = "Identifier for the Coder external auth provider. The associated access token will be used to configure git."
+  type        = string
+}
+
 variable "set_gh_token" {
   description = "If true, sets the GH_TOKEN environment variable to the configured access token"
   type        = bool
   default     = true
-}
-
-variable "external_auth_id" {
-  description = "Identifier for the Coder external auth provider. The associated access token will be used to configure git."
-  type        = string
 }
 
 data "coder_external_auth" "this" {
@@ -30,14 +30,14 @@ data "coder_external_auth" "this" {
 }
 
 resource "coder_env" "gh_token" {
-  count = var.set_gh_token ? 1 : 0
+  count    = var.set_gh_token ? 1 : 0
   agent_id = var.agent_id
-  name = "GH_TOKEN"
-  value = data.coder_external_auth.this.access_token
+  name     = "GH_TOKEN"
+  value    = data.coder_external_auth.this.access_token
 }
 
 resource "coder_script" "this" {
-  agent_id = var.agent_id
+  agent_id           = var.agent_id
   display_name       = "GH Git Config"
   run_on_start       = true
   start_blocks_login = true

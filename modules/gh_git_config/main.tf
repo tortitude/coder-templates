1329753b61d16d02956ba9893902653d162c8c67
TylerHendrickson <1851017+TylerHendrickson@users.noreply.github.com>
2024-10-24 23:00:43 +0000

    Module to git config using GitHub user details

---
+++ modules/gh_git_config/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = ">= 1.0"
+    }
+  }
+}
+
+variable "agent_id" {
+  description = "The ID of a Coder agent."
+  type        = string
+}
+
+variable "set_gh_token" {
+  description = "If true, sets the GH_TOKEN environment variable to the configured access token"
+  type        = bool
+  default     = true
+}
+
+variable "external_auth_id" {
+  description = "Identifier for the Coder external auth provider. The associated access token will be used to configure git."
+  type        = string
+}
+
+data "coder_external_auth" "this" {
+  id = var.external_auth_id
+}
+
+resource "coder_env" "gh_token" {
+  count = var.set_gh_token ? 1 : 0
+  agent_id = var.agent_id
+  name = "GH_TOKEN"
+  value = data.coder_external_auth.this.access_token
+}
+
+resource "coder_script" "this" {
+  agent_id = var.agent_id
+  display_name       = "GH Git Config"
+  run_on_start       = true
+  start_blocks_login = true
+
+  script = templatefile("${path.module}/run.sh", {
+    GH_TOKEN = data.coder_external_auth.this.access_token
+  })
+}
---
+++ modules/gh_git_config/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+DATA_FILE=$(mktemp)
+
+if [ command -v gh ]; then
+    echo "Using GitHub CLI to query user data"
+    GH_TOKEN="${GH_TOKEN}" gh api /user -q '{ id: .id, login: .login }' >$DATA_FILE
+else
+    echo "GitHub CLI command gh not found; trying curl with jq instead"
+    API_RESPONSE_FILE=$(mktemp)
+    curl -o $API_RESPONSE_FILE -L \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer ${GH_TOKEN}" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/user
+    jq '{ id: .id, login: .login }' $API_RESPONSE_FILE > $DATA_FILE
+    rm $API_RESPONSE_FILE
+fi
+
+GITHUB_LOGIN=$(jq --raw-output .login $DATA_FILE)
+GITHUB_UID=$(jq --raw-output .id $DATA_FILE)
+rm $DATA_FILE
+GITHUB_EMAIL_ALIAS="$GITHUB_UID+$GITHUB_LOGIN@users.noreply.github.com"
+
+echo "Configuring git global user name with GitHub login $GITHUB_LOGIN..."
+git config --global user.name "$GITHUB_LOGIN"
+echo "Configuring git global user email with with anonymized GitHub email $GITHUB_EMAIL_ALIAS..."
+git config --global user.email "$GITHUB_EMAIL_ALIAS"

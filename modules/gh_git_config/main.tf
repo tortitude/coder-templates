c342bbc2a1abf39c310e56bc6adf63195d09afde
TylerHendrickson <1851017+TylerHendrickson@users.noreply.github.com>
2024-10-25 00:28:49 +0000

    clean up

--- modules/gh_git_config/main.tf
+++ modules/gh_git_config/main.tf
@@ -26,8 +26,12 @@ variable "coder_parameter_order" {
   default     = null
 }
 
-data "coder_parameter" "git_config_use_gh" {
-  count = var.external_auth_id != null ? 1 : 0
+locals {
+  maybe_ignored_paramter_notice = try("Ignored when `${data.coder_parameter.git_config_use_gh[0].display_name}` is selected.", null)
+}
+
+data "coder_parameter" "use_gh" {
+  count = var.external_auth_id != null
 
   name    = "git_config_use_gh"
   order   = var.coder_parameter_order != null ? var.coder_parameter_order + 0 : null
@@ -38,24 +42,31 @@ data "coder_parameter" "git_config_use_gh" {
   description  = "Use the workspace owner's GitHub to configure git user.name and user.email (anonymized `@users.noreply.github.com` address)."
 }
 
-data "coder_parameter" "git_config_user_name" {
+locals {
+  maybe_ignored_paramter_notice = try(
+    "Ignored when `${data.coder_parameter.git_config_use_gh[0].display_name}` is selected.",
+    null
+  )
+}
+
+data "coder_parameter" "git_user_name" {
   name    = "git_config_user_name"
   order   = var.coder_parameter_order != null ? var.coder_parameter_order + 1 : null
   type    = "string"
   default = ""
 
-  display_name = "Git config user name"
-  description  = "Ignored when `${data.coder_parameter.git_config_use_gh.display_name}` is selected."
+  display_name = "Git config user.name"
+  description  = local.maybe_ignored_paramter_notice
 }
 
-data "coder_parameter" "git_config_user_email" {
+data "coder_parameter" "git_user_email" {
   name    = "git_config_user_email"
   order   = var.coder_parameter_order != null ? var.coder_parameter_order + 2 : null
   type    = "string"
   default = ""
 
-  display_name = "Git config user email"
-  description  = "Ignored when `${data.coder_parameter.git_config_use_gh.display_name}` is selected."
+  display_name = "Git config user.email"
+  description  = local.maybe_ignored_paramter_notice
 }
 
 data "coder_parameter" "set_gh_token_env" {
@@ -76,9 +87,9 @@ data "coder_external_auth" "this" {
 locals {
   gh_token            = one(data.coder_external_auth.this[*].access_token)
   set_gh_token_env    = tobool(data.coder_parameter.set_gh_token_env.value) && var.external_auth_id != null
-  use_gh              = tobool(try(data.coder_parameter.git_config_use_gh[0].value, false))
-  provided_user_name  = try(coalesce(data.coder_parameter.git_config_user_name.value), "")
-  provided_user_email = try(coalesce(data.coder_parameter.git_config_user_email.value), "")
+  use_gh              = try(tobool(data.coder_parameter.use_gh[0].value), false)
+  provided_user_name  = try(coalesce(data.coder_parameter.git_user_name.value), "")
+  provided_user_email = try(coalesce(data.coder_parameter.git_user_email.value), "")
 }
 
 resource "coder_env" "gh_token" {

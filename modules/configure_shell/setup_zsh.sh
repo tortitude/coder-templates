295bcbff8276ea1d185dd41c1e71e1e47579b53b
tyler <1851017+TylerHendrickson@users.noreply.github.com>
2024-11-01 23:54:09 +0000

    Remove debugging prints

--- modules/configure_shell/setup_zsh.sh
+++ modules/configure_shell/setup_zsh.sh
@@ -6,8 +6,6 @@ if [[ ! -d "$HOME/.oh-my-zsh/" ]]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 fi
 
-echo "${OMZ_PLUGINS}"
-echo "Plugin: ${plugin_name}"
 <<- 'EOF' | zsh -s
 source $HOME/.zshrc
 omz plugin enable ${OMZ_PLUGINS} || true
